de811fa94f1375589ff3cd07bce38752509fa5d0
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2021-11-28 21:21:33 +0100

    main.c: add dialog for user to choose between logging in and creating new user

--- main.c
+++ main.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "users/users.h"
+#include <limits.h>
 
-int main()
-{
+int main() {
+    char dialog_buf[128];
     char login[255], password[255];
+
+    puts("1) Login to account\n2) Create account");
+
+    // Get value from stdin and check for errors
+    fgets(dialog_buf, 128, stdin);
+    long dialog_value = strtol(dialog_buf, NULL, 0);
+    printf("%ld\n", dialog_value);
+    if (dialog_value == LONG_MIN || dialog_value == LONG_MAX) {
+        exit(-1);
+    }
+
+    if (dialog_value != 1 && dialog_value != 2) {
+        puts("\nQuitting...");
+        exit(0);
+    }
 
     puts("Enter login");
     scanf("%s", login);
     puts("Enter password");
     scanf("%s", password);
 
-    if(authentication(login, password))
-        puts("\nAuthentication successful");
-    else
-        puts("\nAuthentication failed");
+    if (dialog_value == 1) {
+        if (authentication(login, password))
+            puts("\nAuthentication successful");
+        else
+            puts("\nAuthentication failed");
+    } else {
+        if (create_user(login, password)) {
+            puts("\nAccount successfully created");
+        } else {
+            puts("\nUsername is already taken");
+        }
+    }
+
     return 0;
 
 }
