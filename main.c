17147390d8a609dd849d2a2d421bdb7ffa61080d
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2021-12-02 17:56:18 +0100

    add validation for login, password

--- main.c
+++ main.c
@@ -15,24 +15,50 @@ int main() {
     if (dialog_value == LONG_MIN || dialog_value == LONG_MAX) {
         exit(-1);
     }
-
     if (dialog_value != 1 && dialog_value != 2) {
         puts("\nQuitting...");
         exit(0);
     }
 
-    puts("Enter login");
-    scanf("%s", login);
-    puts("Enter password");
-    scanf("%s", password);
 
     if (dialog_value == 1) {
+        puts("Enter login: ");
+        scanf("%s", login);
+        puts("Enter password: ");
+        scanf("%s", password);
         if (authentication(login, password))
             puts("\nAuthentication successful");
         else
             puts("\nAuthentication failed");
     } else {
-        if (create_user(login, password)) {
+        puts("\nEnter login: ");
+        scanf("%s", login);
+        while(strlen(login) < 6){
+            puts("\nLogin must be at least 6 letters long\nEnter login: ");
+            scanf("%s", login);
+        }
+        puts("\nEnter password: ");
+        scanf("%s", password);
+
+        int password_validation = validate_password(password);
+        while(password_validation != 1){
+            switch(password_validation){
+                case -1:
+                    puts("Password must be at least 10 characters long");
+                    break;
+                case -2:
+                    puts("Password must have at least 1 uppercase letter");
+                    break;
+                case -3:
+                    puts("Password must have at least 1 special symbol");
+                    break;
+            }
+            puts("\nEnter password: ");
+            scanf("%s", password);
+            password_validation = validate_password(password);
+        }
+
+        if (create_user(login, password) ) {
             puts("\nAccount successfully created");
         } else {
             puts("\nUsername is already taken");
--- users/users.h
+++ users/users.h
@@ -1,6 +1,14 @@
 #ifndef BANK_USERS_H
 #define BANK_USERS_H
+
 #include <string.h>
+
+const char password_symbols[31] = {'~', '`', '!', ' ', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=',
+                                   '{',
+                                   '[', '}', ']', '|', '\\', ':', ';', '<', ',', '>', '.', '?', '/'};
+
+const char uppercase_letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                                    'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 /*
  Authenticate user using db.txt file and provided arguments
@@ -11,26 +19,28 @@
     0 if provided data is not matching db.txt data
 
  */
-int authentication(char login[], char password[]){
+int authentication(char login[], char password[]) {
     char line[255];
     int result;
-    FILE * pFile = fopen("db.txt", "r");
+    FILE *pFile = fopen("db.txt", "r");
     result = fscanf(pFile, "%s", line);
-    while(result != EOF){
-        if(strcmp(line, login) == 0){
+    while (result != EOF) {
+        if (strcmp(line, login) == 0) {
             fscanf(pFile, "%s", line); // gets next line (password)
             fclose(pFile);
-            if(strcmp(line, password) == 0)
+            if (strcmp(line, password) == 0)
                 return 1;
             return 0;
         }
-        fscanf(pFile, "%s", line); // this line is password, it gets skipped because we don't need it if login is incorrect
+        fscanf(pFile, "%s",
+               line); // this line is password, it gets skipped because we don't need it if login is incorrect
         result = fscanf(pFile, "%s", line);
     }
     fclose(pFile);
 
     return 0;
 }
+
 /*
  * Check if user with provided login exists
  *
@@ -39,16 +49,17 @@ int authentication(char login[], char password[]){
  *      0 if user doesn't exist
  *
  * */
-int check_user(FILE * pFile, char login[]){
+int check_user(FILE *pFile, char login[]) {
     char line[255];
     int result = fscanf(pFile, "%s", line);
-    while(result != EOF){
-        if(strcmp(line, login) == 0)
+    while (result != EOF) {
+        if (strcmp(line, login) == 0)
             return 1;
         result = fscanf(pFile, "%s", line);
     }
     return 0;
 }
+
 /*
  * Creates user if user with provided login doesn't exist
  *
@@ -58,17 +69,48 @@ int check_user(FILE * pFile, char login[]){
  *
  * */
 
-int create_user(char login[], char password[]){
-    FILE * pFile = fopen("db.txt", "a+");
-    if(check_user(pFile, login) == 0){
+int create_user(char login[], char password[]) {
+    FILE *pFile = fopen("db.txt", "a+");
+    if (check_user(pFile, login) == 0) {
         fprintf(pFile, "%s\n%s\n\n", login, password);
-    }
-    else {
+    } else {
         return 0;
     }
     fclose(pFile);
     return 1;
 }
 
+/* Validate password from input
+ * Password rules:
+ *  - at least 10 characters
+ *  - at least 1 special symbol from password_symbols
+ *  - at least 1 uppercase letter
+ *
+ *  Return value:
+ *  1: password meets requirements
+ *  -1: password is too short
+ *  -2: password doesn't have any uppercase letter
+ *  -3: password doesn't have any special symbol
+ *
+ * */
+int validate_password(char password[]){
+    int containsUppercaseLetter = 0;
+    if (strlen(password) < 10)
+        return -1;
+    for(int i = 0; i < 26; i++){
+        if(strchr(password, uppercase_letters[i]) != NULL){
+            containsUppercaseLetter = 1;
+            break;
+        }
+    }
+    if(!containsUppercaseLetter){
+        return -2;
+    }
+    for (int i = 0; i < 31; i++) {
+        if (strchr(password, password_symbols[i]) != NULL)
+            return 1;
+    }
+    return -3;
+}
 
 #endif
