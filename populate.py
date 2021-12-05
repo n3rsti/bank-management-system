af8842951b67d93925f840d5e65694a232ad24f3
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2021-12-05 14:21:13 +0100

    populate.py: edit script so generated data is always the same. it's done so new tests doesn't need to be generated every populate execution tests.c: update test data

--- populate.py
+++ populate.py
@@ -2,7 +2,7 @@ import string
 import random
 
 names = "Liam Noah Oliver Elijah William James Benjamin Lucas Henry Alexander Mason Michael Ethan Daniel Jacob Logan Jackson Levi Sebastian Mateo Jack Owen Theodore Aiden Samuel Joseph John David Wyatt Matthew Luke Asher Carter Julian Grayson Leo Jayden Gabriel Isaac Lincoln Anthony Hudson Dylan Ezra Thomas Charles Christopher Jaxon Maverick Josiah Isaiah Andrew Elias Joshua Nathan Caleb Ryan Adrian Miles Eli Nolan Christian Aaron Cameron Ezekiel Colton Luca Landon Hunter Jonathan Santiago Axel Easton Cooper Jeremiah Angel Roman Connor Jameson Robert Greyson Jordan Ian Carson Jaxson Leonardo Nicholas Dominic Austin Everett Brooks Xavier Kai Jose Parker Adam Jace Wesley Kayden Silas".split(" ")
-
+passwords = "oD5Mm6r#vw kM1L5K6e7g ToBEdd#ohX J)YlEvNzVA 3!aEuFEmSX LbmxIF9I&m 9#5#%2(TF@ RwX0g&VvP* %odn0@@NCD IrI*ST@EM2 9jNocX@$%& NFakNBq(Xx f9KE1Z62BZ YRVXt6utGf pcuM6MaZUq HTN$Aeqc^9 )T*7MAevTo GkrqOjyyY! 9lGUrxEaNH UmcdQX^rOa oLxwSjDdGi U8hqlrJPKl S@mJXvBjsU ^XV@F!md&@ 6mCys1M&zf svGsV1VTK) &TABrS8%%i FLiz^N@At6 ^qQB&nVdvS 7GQiJ$vxCX 9TqXncUyYH wm4Ug9Jsjl gH%!^*&d&# eW2m9dS8U1 SZIXH9iDVU OK2WhG3DEm $f%Nre3Z@G uu#%gQkieN R!1rE!Bs(@ a3$voCoc74 nyL!Qx&B&! T(rK1ottMW %uq#*L^rU@ 6Oh(HTPihY 8mKAy)qbx! *(Xl7RoNUo Dg)RN833V& u(*RCz0Y7Z fyf)q9SSeP pN(mzPYu@g VPGy47EVp* FoQn%U0uG* u8c4MC#Ffw WFQ$h1a6qQ M*9gp#YBqM !)A4N0NDyp maHYUXnp2H okeO#EAY9g o7!^sa8SYu &@a%cl5zj& jpfObr50#m (043jfXaHs tKq8qF37h% Gg5#$x90o1 6r#sdpN!VA OP7Z@M0XLj 0l9Tr2QwMF (NOUTPWP*G iW5!lHalKA Pzr%uzWAV6 H)G$dc&QGa DkeowvOuk4 86oZntH6nW 7b7Hv9FiU4 8sY*#oSoEP hO3RZKPfQo OrACZfvOg* O$(rN0MI^o C2aVZSLIiD @IL*LTLGKp SKGweOWgk3 WN%hzSrbuS XvZCjHEOl# txpB&!x@s@ 4Anl3Z0me1 &)yt6NzpEb ap3Rkdijxy 2BbtmjBcSE bD7%QF*%GG dCJpCqipg) rB^r30y@x) X1pn(2VBVa v%D8Um0S)0 FZl!C5l)AB muTXSwHO&V tIinwVliWU s*HP)uciL* htNHOOkt7v 2F^o&DF$ei 6FX33p$XM*".split(" ")
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 def generate_random_password():
@@ -16,7 +16,12 @@ def generate_random_password():
     random.shuffle(password)
     return "".join(password)
 
-with open("db.txt", "w") as file:
+with open("data/db.txt", "w") as file:
     for x in range(100):
-        file.write(f"{x+1}\n{names[x]}\n{generate_random_password()}\n\n")
+        file.write(f"{x+1}\n{names[x]}\n{passwords[x]}\n\n")
+    file.close()
+
+with open("data/balance.txt", "w") as file:
+    for x in range(100):
+        file.write(f"{x+1}\n{round(random.randint(0, 10000), 2)}\n\n")
     file.close()
--- tests.c
+++ tests.c
@@ -1,41 +1,72 @@
 #include <stdio.h>
 #include "users/users.h"
 
-char * authentication_true_cases[39][2] = {
-        {"Liam", "%r7l@bM#Jf"}, {"Noah", "r!w(8Q1Su5"}, {"Oliver", "ZZph3JUu*)"},
-        {"Elijah", "gDxhef9FGZ"}, {"William", "JYPC@oKnV!"}, {"James", "e3ARa6mhxJ"},
-        {"Benjamin", "P!Zz6cToJ&"}, {"Lucas", "vNra#q$1SA"}, {"Henry", "eOPu0uBTVO"},
-        {"Alexander", "%IKjdH)EwI"}, {"Mason", "geCHA6Wj)e"}, {"Michael", "vmDfxul(s#"},
-        {"Ethan", "wUApfj$DYC"}, {"Daniel", "rLM(yhSMRU"}, {"Jacob", "W6KjnJWdtA"},
-        {"Logan", "QndJ(Z9FRV"}, {"Jackson", "x!4jjZTL^S"}, {"Levi", "YmFcPDzmt0"},
-        {"Sebastian", "ihZOC(UR@T"}, {"Mateo", "0lgn4feBqk"}, {"Jack", "ulfaoDum$E"},
-        {"Owen", "**JA@Ml2P1"}, {"Theodore", "CKqnFVpD5#"}, {"Aiden", "irFd9D%Z0x"},
-        {"Samuel", "wEIrNsU71("}, {"Joseph", "d3F@sF&^Y@"}, {"John", "FzESXEMNhq"},
-        {"David", "wBSYo1Seww"}, {"Wyatt", "g6f6w4Mgmy"}, {"Matthew", "1G!fWt&*0o"},
-        {"Luke", "PUfOmGgrBl"}, {"Asher", "!tDMgwmE1S"}, {"Carter", "Cqk^lP9aPr"},
-        {"Julian", "CtOaU3WJYk"}, {"Grayson", "43DBb&lX0q"}, {"Leo", "v79265QC1n"},
-        {"Jayden", "Oy3TmsP2mg"}, {"Gabriel", "PpT7c^9hx^"}, {"Isaac", "@k(PEK2%d*"}
+// data
+char *authentication_true_cases[39][2] = {
+        {"Liam",      "oD5Mm6r#vw"},
+        {"Noah",      "kM1L5K6e7g"},
+        {"Oliver",    "ToBEdd#ohX"},
+        {"Elijah",    "J)YlEvNzVA"},
+        {"William",   "3!aEuFEmSX"},
+        {"James",     "LbmxIF9I&m"},
+        {"Benjamin",  "9#5#%2(TF@"},
+        {"Lucas",     "RwX0g&VvP*"},
+        {"Henry",     "%odn0@@NCD"},
+        {"Alexander", "IrI*ST@EM2"},
+        {"Mason",     "9jNocX@$%&"},
+        {"Michael",   "NFakNBq(Xx"},
+        {"Ethan",     "f9KE1Z62BZ"},
+        {"Daniel",    "YRVXt6utGf"},
+        {"Jacob",     "pcuM6MaZUq"},
+        {"Logan",     "HTN$Aeqc^9"},
+        {"Jackson",   ")T*7MAevTo"},
+        {"Levi",      "GkrqOjyyY!"},
+        {"Sebastian", "9lGUrxEaNH"},
+        {"Mateo",     "UmcdQX^rOa"},
+        {"Jack",      "oLxwSjDdGi"},
+        {"Owen",      "U8hqlrJPKl"},
+        {"Theodore",  "S@mJXvBjsU"},
+        {"Aiden",     "^XV@F!md&@"},
+        {"Samuel",    "6mCys1M&zf"},
+        {"Joseph",    "svGsV1VTK)"},
+        {"John",      "&TABrS8%%i"},
+        {"David",     "FLiz^N@At6"},
+        {"Wyatt",     "^qQB&nVdvS"},
+        {"Matthew",   "7GQiJ$vxCX"},
+        {"Luke",      "9TqXncUyYH"},
+        {"Asher",     "wm4Ug9Jsjl"},
+        {"Carter",    "gH%!^*&d&#"},
+        {"Julian",    "eW2m9dS8U1"},
+        {"Grayson",   "SZIXH9iDVU"},
+        {"Leo",       "OK2WhG3DEm"},
+        {"Jayden",    "$f%Nre3Z@G"},
+        {"Gabriel",   "uu#%gQkieN"},
+        {"Isaac",     "R!1rE!Bs(@"},
 };
-char * authentication_false_cases[7][2] = {
-        {"John", "123"},
-        {"Bob", "1"},
-        {"3", "Liam"},
+char *authentication_false_cases[7][2] = {
+        {"John",    "123"},
+        {"Bob",     "1"},
+        {"3",       "Liam"},
         {"sdsJohn", "123"},
-        {"Basdob", "1"},
-        {"sd3", "Lia2m"},
-        {"", "s"},
+        {"Basdob",  "1"},
+        {"sd3",     "Lia2m"},
+        {"",        "s"},
 };
 
+
+// Tests
 int main() {
     for (int i = 0; i < 39; i++) {
         if (authentication(authentication_true_cases[i][0], authentication_true_cases[i][1]) == 0) {
-            printf("Auth true case failed for: %s %s\n", authentication_true_cases[i][0], authentication_true_cases[i][1]);
+            printf("Auth true case failed for: %s %s\n", authentication_true_cases[i][0],
+                   authentication_true_cases[i][1]);
             break;
         }
     }
     for (int i = 0; i < 7; i++) {
         if (authentication(authentication_false_cases[i][0], authentication_false_cases[i][1]) >= 1) {
-            printf("Auth false case failed for: %s %s\n", authentication_false_cases[i][0], authentication_false_cases[i][1]);
+            printf("Auth false case failed for: %s %s\n", authentication_false_cases[i][0],
+                   authentication_false_cases[i][1]);
             break;
         }
     }
